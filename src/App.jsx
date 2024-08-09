0aeec9ff8e3c8382400f4a5f98dae91e5e9da861
Abdulholiq13 <abdulholiq571@gmail.com>
2024-08-09 15:46:24 +0500

    add skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import HeaderTop from './components/HeaderTop/HeaderTop';
 import Intro from './components/Intro/Intro';
 import PopularCategory from './components/PopularCategory/PopularCategory';
+import Sale from './components/Sale/Sale';
 
 const App = () => {
 	return (
@@ -8,6 +9,7 @@ const App = () => {
 			<HeaderTop />
 			<Intro />
 			<PopularCategory />
+			<Sale />
 		</>
 	);
 };
--- src/components/Sale/Sale.jsx
+++ src/components/Sale/Sale.jsx
@@ -1,11 +1,10 @@
 import { useState, useEffect } from 'react';
 import { FaArrowRightLong } from 'react-icons/fa6';
 import axios from 'axios';
-import CardSkeleton from '../Skeleton/CardSkeleton';
 
 const endpoint = 'https://dummyjson.com';
 
-const PopularCategory = () => {
+const Sale = () => {
 	const [products, setProducts] = useState(null);
 	const [loading, setLoading] = useState(true);
 
@@ -19,33 +18,33 @@ const PopularCategory = () => {
 			.catch((err) => console.error(err));
 	}, []);
 
-	const productItem = products?.slice(0, 10).map((product) => (
+	const productCard = products?.slice(10, 14).map((product) => (
 		<div
 			key={product.id}
 			className='shadow-inset rounded-[30px] pt-5 px-3 pb-3 flex flex-col items-center justify-center'
 		>
 			<div>
-				<img className='mx-auto object-contain w-[105px] h-[129px]' src={product.images[0]} alt='' />
+				<img className='mx-auto object-contain w-[289px] h-[291px]' src={product.images[0]} alt='' />
 			</div>
+			<p>12%</p>
 			<h3 className='text-center inline-block '>{product.title}</h3>
 		</div>
 	));
 
 	return (
-		<div className='container pb-[60px]'>
-			<div className='flex items-end justify-start gap-5 mb-[42px]'>
-				<h2 className='font-jost text-4xl leading-8 font-bold'>Популярные категории</h2>
+		<div className='container'>
+			<div className='flex items-end justify-start gap-5 mb-[35px]'>
+				<h2 className='font-jost text-4xl leading-8 font-bold'>
+					Скидки <span className='text-[#FF3232] font-normal'>%</span>
+				</h2>
 				<span className='flex items-center gap-3 text-lg'>
-					Все категории <FaArrowRightLong className='text-base' />
+					Все товары в категории <FaArrowRightLong className='text-base' />
 				</span>
 			</div>
 
-			<div className='grid grid-cols-5 gap-6'>
-				{loading && <CardSkeleton cards={10} />}
-				{productItem}
-			</div>
+			<div className='grid grid-cols-4 gap-6'>{productCard}</div>
 		</div>
 	);
 };
 
-export default PopularCategory;
+export default Sale;
